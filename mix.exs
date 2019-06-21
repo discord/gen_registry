e0e55418db7d5da35b86667fe9527b84f02c9ccb
matt <matt@discordapp.com>
2019-06-21 13:04:50 -0700

    Bump version for new typespec

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GenRegistry.Mixfile do
   def project do
     [
       app: :gen_registry,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,

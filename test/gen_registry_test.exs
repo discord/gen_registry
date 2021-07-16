cfb6f62a03d9997ac1a67da7d2f1ba68238e30ad
Matt Nowack <matt@discordapp.com>
2021-07-15 19:21:31 -0700

    Encode a common pattern as the default behavior. (#6)
    * Encode a common pattern as the default behavior.
    
    It is a common pattern when using GenRegistry to wrap lookup_or_start/4
    with a call to lookup/2 first.
    
    This pattern exists because it allows the caller to do a fast ETS
    lookup in the client context without having to switch to the server
    context when the process is already running.
    
    lookup_or_start/4 accepts a GenServer.server(), in the cases that a
    caller uses a pid, the old path will be followed since there's no good
    way to go from pid to :ets.tab().  If the caller provides an atom
    (local named process) then the atom will be assumed to be the
    :ets.tab() and a client-side lookup performed.
    
    * Tests for the non-name case

--- test/gen_registry_test.exs
+++ test/gen_registry_test.exs
@@ -11,8 +11,8 @@ defmodule GenRegistry.Test do
   """
   @spec start_registry(ctx :: Map.t()) :: :ok
   def start_registry(_) do
-    {:ok, _} = start_supervised({GenRegistry, worker_module: ExampleWorker})
-    :ok
+    {:ok, registry} = start_supervised({GenRegistry, worker_module: ExampleWorker})
+    {:ok, registry: registry}
   end
 
   @doc """
@@ -183,6 +183,74 @@ defmodule GenRegistry.Test do
 
       # Confirm that the registry has 1 process
       assert 1 == GenRegistry.count(ExampleWorker)
+    end
+
+    test "when given a local name will perform a client-side lookup" do
+      # Start a process so something will be available client-side
+      assert {:ok, pid} = GenRegistry.lookup_or_start(ExampleWorker, :test_id, [:test])
+
+      # Replace the GenRegistry with a Spy
+      assert {:ok, spy} = Spy.replace(ExampleWorker)
+
+      # Assert that the spy has seen 0 calls
+      assert {:ok, []} == Spy.calls(spy)
+
+      # Assert that the GenRegistry can lookup the `:test_id` process
+      assert {:ok, pid} == GenRegistry.lookup_or_start(ExampleWorker, :test_id, [:test])
+
+      # Assert again that the spy has seen 0 calls
+      assert {:ok, []} == Spy.calls(spy)
+    end
+
+    test "when given a local name will perform a server-side lookup and start if id not found" do
+      # Replace the GenRegistry with a Spy
+      assert {:ok, spy} = Spy.replace(ExampleWorker)
+
+      # Assert that the spy has seen 0 calls
+      assert {:ok, []} == Spy.calls(spy)
+
+      # Assert that the GenRegistry can still perform a lookup_or_start
+      assert {:ok, pid} = GenRegistry.lookup_or_start(ExampleWorker, :test_id, [:test])
+
+      # Assert that the spy saw the call for lookup_or_start
+      assert {:ok, [{call, response}]} = Spy.calls(spy)
+
+      # Assert that the call is what we would expect for a server-side lookup_or_start
+      assert {:lookup_or_start, :test_id, [:test]} == call
+
+      # Assert that the response is the one returned from the call
+      assert {:ok, pid} == response
+    end
+
+    test "when given a pid will perform a server-side lookup and start if id is running", ctx do
+      # Start a process so something will be available client-side
+      assert {:ok, pid} = GenRegistry.lookup_or_start(ExampleWorker, :test_id, [:test])
+
+      # Replace the GenRegistry with a Spy
+      assert {:ok, spy} = Spy.replace(ctx.registry)
+
+      # Assert that the spy has seen 0 calls
+      assert {:ok, []} == Spy.calls(spy)
+
+      # Assert that the GenRegistry can lookup the `:test_id` process
+      assert {:ok, pid} == GenRegistry.lookup_or_start(spy, :test_id, [:test])
+
+      # Assert again that the spy has seen 0 calls
+      assert {:ok, [{call, response}]} = Spy.calls(spy)
+    end
+
+    test "when given a pid will perform server-side lookup and start if id is not running", ctx do
+      # Replace the GenRegistry with a Spy
+      assert {:ok, spy} = Spy.replace(ctx.registry)
+
+      # Assert that the spy has seen 0 calls
+      assert {:ok, []} == Spy.calls(spy)
+
+      # Assert that the GenRegistry can lookup the `:test_id` process
+      assert {:ok, pid} = GenRegistry.lookup_or_start(spy, :test_id, [:test])
+
+      # Assert again that the spy has seen 0 calls
+      assert {:ok, [{call, response}]} = Spy.calls(spy)
     end
   end
 
